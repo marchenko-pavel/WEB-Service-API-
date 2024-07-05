4eff4f5c9cdd4c2153d4650c7ae1e8bb8f7f68d4
Pavel Marchenko <marchenko.pawel@gmail.com>
2024-07-05 21:39:42 +0300

    Исправил нарушение атомарности добавления Точки измерения в контроллере.

--- Web/Controllers/EnergyController.cs
+++ Web/Controllers/EnergyController.cs
@@ -1,8 +1,8 @@
 ﻿using Infrastructure.DAL;
 using Infrastructure.Data.EfModels;
 using Microsoft.AspNetCore.Mvc;
-using System.Text;
 using Web.Models;
+using Web.Static;
 
 namespace Web.Controllers;
 
@@ -87,80 +87,13 @@ public class EnergyController : ControllerBase
     [HttpPost("AddMeasuringPoint")]
     public async Task<IActionResult> AddMeasuringPointAsync([FromBody] MeasuringPointModel body)
     {
-        MeasuringPoint? measurePoin = null;
-        StringBuilder log = new();
-        string succedMsg = "устешно дабавлен";
-        string failMsg = "не добавлен";
-
         try
         {
-            var isSucced = await AddMeasurPointAsync(body);
-            if (isSucced) measurePoin = await _repository.GetMeasuringPointAsync(body.Name);
-
-            if (measurePoin is not null)
-            {
-                log.Append($"Данные операции: точка измерения '{body.Name}' - {succedMsg}; \n");
-
-                isSucced = await AddElectricMeterAsync(measurePoin.Id, body);
-                log.Append($"счетчик №'{body.ElectricMeter.InventoryNumber}' - " + (isSucced ? succedMsg : failMsg) + "; \n");
-
-                isSucced = await AddCurrentTransformerAsync(measurePoin.Id, body);
-                log.Append($"трансформатор тока №'{body.CurrentTransformer.InventoryNumber}' - " + (isSucced ? succedMsg : failMsg) + "; \n");
-
-                isSucced = await AddVoltageTransformerAsync(measurePoin.Id, body);
-                log.Append($"трансформатор напряжения №'{body.VoltageTransformer.InventoryNumber}' - " + (isSucced ? succedMsg : failMsg));
-
-                return Ok(log.ToString());
-            }
-            else { return BadRequest($"Не удалось добавить в БД точку измерения '{body.Name}'"); }
+            return Ok(await _repository.AddObjectAsync<MeasuringPoint>(body.GetMeasuringPoint()));
         }
         catch (Exception ex)
         {
-            return BadRequest(String.Concat(ex.InnerException?.Message, "\n", log.ToString()));
+            return BadRequest(ex.InnerException?.Message);
         }
-    }
-    private async Task<bool> AddMeasurPointAsync(MeasuringPointModel body)
-    {
-        return await _repository.AddObjectAsync<MeasuringPoint>(
-            new MeasuringPoint()
-            {
-                Name = body.Name,
-                ConsumptionObjectId = body.ConsumptionObjectId
-            });
-    }
-    private async Task<bool> AddElectricMeterAsync(int measurePoinId, MeasuringPointModel body)
-    {
-        return await _repository.AddObjectAsync<ElectricMeter>(
-            new ElectricMeter()
-            {
-                Id = measurePoinId,
-                InventoryNumber = body.ElectricMeter.InventoryNumber,
-                TypeId = body.ElectricMeter.TypeId,
-                Verificated = DateOnly.FromDateTime(body.ElectricMeter.Verificated)
-            });
-    }
-    private async Task<bool> AddCurrentTransformerAsync(int measurePoinId, MeasuringPointModel body)
-    {
-        return await _repository.AddObjectAsync<CurrentTransformer>(
-            new CurrentTransformer()
-            {
-                Id = measurePoinId,
-                InventoryNumber = body.CurrentTransformer.InventoryNumber,
-                TypeId = body.CurrentTransformer.TypeId,
-                Verificated = DateOnly.FromDateTime(body.CurrentTransformer.Verificated),
-                Kt = body.CurrentTransformer.Kt
-            });
-    }
-    private async Task<bool> AddVoltageTransformerAsync(int measurePoinId, MeasuringPointModel body)
-    {
-        return await _repository.AddObjectAsync<VoltageTransformer>(
-            new VoltageTransformer()
-            {
-                Id = measurePoinId,
-                InventoryNumber = body.VoltageTransformer.InventoryNumber,
-                TypeId = body.VoltageTransformer.TypeId,
-                Verificated = DateOnly.FromDateTime(body.VoltageTransformer.Verificated),
-                Kt = body.VoltageTransformer.Kt
-            });
     }
 }
---
+++ Web/Static/Extensions.cs
@@ -0,0 +1,45 @@
+﻿using Infrastructure.Data.EfModels;
+using Web.Models;
+
+namespace Web.Static;
+public static class Extensions
+{
+    public static MeasuringPoint GetMeasuringPoint(this MeasuringPointModel model)
+    {
+
+        var measuringPoint = new MeasuringPoint() { Name = model.Name, ConsumptionObjectId = model.ConsumptionObjectId };
+        measuringPoint.ElectricMeter = GetElectricMeter(model);
+        measuringPoint.CurrentTransformer = GetCurrentTransformer(model);
+        measuringPoint.VoltageTransformer = GetVoltageTransformer(model);
+        return measuringPoint;
+    }
+    private static ElectricMeter GetElectricMeter(MeasuringPointModel model)
+    {
+        return new ElectricMeter()
+        {
+            InventoryNumber = model.ElectricMeter.InventoryNumber,
+            TypeId = model.ElectricMeter.TypeId,
+            Verificated = DateOnly.FromDateTime(model.ElectricMeter.Verificated)
+        };
+    }
+    private static CurrentTransformer GetCurrentTransformer(MeasuringPointModel model)
+    {
+        return new CurrentTransformer()
+        {
+            InventoryNumber = model.CurrentTransformer.InventoryNumber,
+            TypeId = model.CurrentTransformer.TypeId,
+            Verificated = DateOnly.FromDateTime(model.CurrentTransformer.Verificated),
+            Kt = model.CurrentTransformer.Kt
+        };
+    }
+    private static VoltageTransformer GetVoltageTransformer(MeasuringPointModel model)
+    {
+        return new VoltageTransformer()
+        {
+            InventoryNumber = model.VoltageTransformer.InventoryNumber,
+            TypeId = model.VoltageTransformer.TypeId,
+            Verificated = DateOnly.FromDateTime(model.VoltageTransformer.Verificated),
+            Kt = model.VoltageTransformer.Kt
+        };
+    }
+}
